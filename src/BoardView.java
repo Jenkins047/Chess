ebca09a6f3134befd7442172ac7594e747eeeff9
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-05-20 23:58:24 +0200

    Switch from MV to MVC design pattern and make class listen to events from TileView

--- src/BoardView.java
+++ src/BoardView.java
@@ -4,43 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BoardView extends JPanel implements ActionListener {
-    private final int xMax   = 400; // Szerokość planszy (piksele).
-    private final int yMax   = 400; // Wysokość planszy (piksele).
-    private final int margin =  30; // Margines wokół planszy (piksele).
 
-    private final Chess model;
+    private final ChessControler controller;
 
-    BoardView(Chess m, GridLayout gridLayout)
+    BoardView(ChessControler controller, GridLayout gridLayout)
     {
         super(gridLayout);
-        model = m;
-
-        Icon whiteTile = new ImageIcon("img/white_tile.png");
-        Icon blackTile = new ImageIcon("img/dark_tile.png");
-        for(Tile tile: model.getTiles())
-        {
-            if(tile.getColor() == Color.WHITE)
-                add(new TileView(tile, whiteTile));
-            else if(tile.getColor() == Color.BLACK)
-                add(new TileView(tile, blackTile));
-        }
+        this.controller = controller;
+        controller.initView(this);
     }
 
     public void actionPerformed(ActionEvent e)
     {
-        TileView target = (TileView) e.getSource();
-
-        switch (model.getState()) {
-            case PLAYER_CHOOSE_FIGURE -> model.PickFigure(target);
-            case PLAYER_MOVE_FIGURE -> model.moveFigure(target);
-            case CHECK_MATE -> model.endGame();
-            default -> {
-            } //throw new IllegalActionException();
-
-
-        }
-        model.goToNextState();
-
-
+        controller.updateModel(((TileView) e.getSource()).getTile());
     }
 }
