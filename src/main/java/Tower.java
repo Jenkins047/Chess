aa79bd388629f0d4f036ba6bead2e414627c0a26
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-05-31 01:01:42 +0200

    Implement getMoves

---
+++ src/main/java/Tower.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+import java.util.Vector;
+
+public class Tower extends Figure{
+    Tower(Color color, HashMap<Tile, Figure> boardState) { super(color, boardState); }
+
+    //TODO implement getMoves
+    @Override
+    public Set<Move> getMoves() {
+        //find chosen tile
+        Point position = null;
+        for(Tile element: boardState.keySet())
+            if(boardState.get(element) == this) {
+                position = element.getPosition();
+                break;
+            }
+        Vector<Move> moveList = new Vector<>();
+        //check +x coordinate
+        for(int x = position.x; x < 7 - position.x; x++)
+        {
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x + x, position.y))), Board.free))
+                moveList.add(new Move(MoveType.NORMAL, new Tile(new Point(position.x + x, position.y))));
+
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x + x, position.y))),
+                    boardState.get(new Tile(new Point(position.x + x, position.y))).getColor() != this.getColor()))
+                            moveList.add(new Move(MoveType.OFFENSIVE, new Tile(new Point(position.x + x, position.y))));
+        }
+
+        //check -x coordinate
+        for(int x = position.x; x >= 0; x--)
+        {
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x + x, position.y))), Board.free))
+                moveList.add(new Move(MoveType.NORMAL, new Tile(new Point(position.x + x, position.y))));
+
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x + x, position.y))),
+                    boardState.get(new Tile(new Point(position.x + x, position.y))).getColor() != this.getColor()))
+                moveList.add(new Move(MoveType.OFFENSIVE, new Tile(new Point(position.x + x, position.y))));
+        }
+
+        //check +y coordinate
+        for(int y = position.y; y >= 0; y--)
+        {
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x, position.y + y))), Board.free))
+                moveList.add(new Move(MoveType.NORMAL, new Tile(new Point(position.x, position.y + y))));
+
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x, position.y + y))),
+                    boardState.get(new Tile(new Point(position.x, position.y + y))).getColor() != this.getColor()))
+                moveList.add(new Move(MoveType.OFFENSIVE, new Tile(new Point(position.x, position.y + y))));
+        }
+
+        //check -y coordinate
+        for(int y = position.y; y < 7; y++)
+        {
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x, position.y + y))), Board.free))
+                moveList.add(new Move(MoveType.NORMAL, new Tile(new Point(position.x, position.y + y))));
+
+            if (Objects.equals(boardState.get(new Tile(new Point(position.x, position.y + y))),
+                    boardState.get(new Tile(new Point(position.x, position.y + y))).getColor() != this.getColor()))
+                moveList.add(new Move(MoveType.OFFENSIVE, new Tile(new Point(position.x, position.y + y))));
+        }
+
+        return Set.copyOf(moveList);
+    }
+}
