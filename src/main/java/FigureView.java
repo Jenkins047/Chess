bfb21e78baddc93ff688c65ef68fba25adbbb3a5
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-05-31 00:58:41 +0200

    Add class

---
+++ src/main/java/FigureView.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FigureView {
+
+    private Figure model;
+
+    FigureView(Tile m, Icon icon)
+    {
+
+    }
+
+    public Figure getFigure() { return this.model; }
+}
