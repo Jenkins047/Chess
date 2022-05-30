0e17724fc3824f1b93477bfb5b92c9cd3687ecbb
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-05-31 00:55:21 +0200

    Implement getters and simplify moveFigure

--- src/main/java/Chess.java
+++ src/main/java/Chess.java
@@ -1,15 +1,17 @@
 import javax.swing.*;
 import java.util.Set;
 
-/*
-*   Klasa nadrzędna, model gry w szachy jako całość, powiązana relacją
-*   kompozytu z klasami Board i Player.
+/**
+*   Klasa nadrzędna, w której modyfikowany jest stan gry,
+ *   metody te są wywoływane przez automat stanów zaimplementowany
+ *   w ChessController
  */
 
 public class Chess implements Runnable {
     private JFrame view;
     private final Board board;
     private Figure pickedFigure;
+    private Set<Move> legalMoves;
     private GameState state;
     private ActivePlayer activePlayer;
     private final int frame_duration_ms;
@@ -24,11 +26,13 @@ public class Chess implements Runnable {
         frame_duration_ms = 25;
     }
 
+    //TODO implement
     private boolean isCheckMate()
     {
         return false;
     }
 
+    private void clearMoves() { legalMoves = Set.of(); }
 
     public void setView(JFrame v) { view = v; }
 
@@ -48,33 +52,31 @@ public class Chess implements Runnable {
         }
     }
 
+    /*TODO display end screen popup after end GameEndException is thrown
+      TODO end screen contains info: which player won, buttons to start new game and to go back to menu
+     */
     private void endGame() {
     }
 
     public void chooseFigure(Tile target) {
         pickedFigure = board.getBoardState().get(target);
-        pickedFigure.getMoves(board.getBoardState());
+        legalMoves = pickedFigure.getMoves();
     }
 
-    public void moveFigure(Tile target) {
-        if(board.getBoardState().get(target) == pickedFigure)
-        {
-            pickedFigure.clearMoves();
-            state = GameState.PLAYER_CHOOSE_FIGURE;
-        }
-        else
-        {
-            board.move(target, pickedFigure);
-            pickedFigure.clearMoves();
+    public void moveFigure(Move move) {
+        if(pickedFigure.getMoves().contains(move)) {
+            board.move(move, pickedFigure);
+            clearMoves();
             pickedFigure = null;
         }
+
     }
 
     public Set<Tile> getTiles()
     {
         return board.getBoardState().keySet();
     }
-    public Figure getTileState(Tile t) { return board.getBoardState().get(t); }
+    public Figure getFigureOnTile(Tile t) { return board.getBoardState().get(t); }
 
     public void run() {
         while(!isCheckMate())
@@ -90,4 +92,13 @@ public class Chess implements Runnable {
     }
 
     public Figure getChosenFigure() { return pickedFigure; }
+
+    public ActivePlayer getActivePlayer() { return this.activePlayer; }
+
+    public Set<Move> getLegalMoves() {
+        if(legalMoves != null)
+            return legalMoves;
+        else
+            return Set.of();
+    }
 }
