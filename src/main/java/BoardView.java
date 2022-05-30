8c8c7184f7e7a844545d827dc6253276c1bb6c90
Szymon Kędzierski <69625790+Jenkins047@users.noreply.github.com>
2022-05-31 00:54:20 +0200

    Add comment

--- src/main/java/BoardView.java
+++ src/main/java/BoardView.java
@@ -3,6 +3,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Widok stanu gry, słuchacz zdarzeń z JButton-ów reprezentujących
+ * pola gry w szachy
+ */
+
 public class BoardView extends JPanel implements ActionListener {
 
     private final ChessController controller;
